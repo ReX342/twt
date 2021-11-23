5691b15340ba167c772af49652d818ab3a36e35f
ReX342 <occ.rex@gmail.com>
2021-11-23 19:50:27 +0100

    index takes another arg because views.py refers id

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
+def index(response, id):
     return HttpResponse("<h1>Tech with Tim</h1>")
