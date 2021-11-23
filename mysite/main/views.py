f26b7b08302988f1a017e746b0576fae118631cc
ReX342 <occ.rex@gmail.com>
2021-11-23 21:42:48 +0100

    weird html tagging and whatnot

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,7 +6,7 @@ from .models import ToDoList, Item
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
     #ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=2)
+    item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
     #return HttpResponse("<h1>%s</h1> %ls.name")
 
