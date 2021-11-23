3fe41e1adb78697c326caadf4462b755568cfcbd
ReX342 <occ.rex@gmail.com>
2021-11-23 21:17:37 +0100

    rexs list works. name instead of id works now

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 
 # Create your views here.
-def index(response, id):
-    ls = ToDoList.objects.get(id=id)
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
     #item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1>" %ls.name)
 
