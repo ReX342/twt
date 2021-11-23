c8fc1e5709e37481c5c6aa1d949ad757db28fc69
ReX342 <occ.rex@gmail.com>
2021-11-23 18:42:04 +0100

    __str__ is simplest reference to model we can read

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -11,3 +11,6 @@ class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     complete = models.BooleanField()
+    
+    def __str__(self):
+        return self.text
