99f5559829c72bd2da8ac0256ddcff8ba4796aba
ReX342 <occ.rex@gmail.com>
2021-11-23 19:53:04 +0100

    surfing to an int path resolves correctly

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(response, id):
-    return HttpResponse("<h1>Tech with Tim</h1>")
+    return HttpResponse("<h1>%s</h1>" % id)
