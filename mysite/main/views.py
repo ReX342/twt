b128253257c2722f9223253e4736dfb63a2c47f0
ReX342 <occ.rex@gmail.com>
2021-11-23 19:48:45 +0100

    38min mark on video removing view after parenthesi

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,6 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(response):
     return HttpResponse("<h1>Tech with Tim</h1>")
-#Copy index(response): and rename definition
-def v1(response):
-    return HttpResponse("<h1>View 1</h1>")
