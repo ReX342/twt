c9529f2af414b30b58ea84a1731278572bc8580f
ReX342 <occ.rex@gmail.com>
2021-11-23 22:51:51 +0100

    item.complete was False to not show items

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -12,9 +12,9 @@ def index(response, id):
         "name":ls.name,
         "ls": ls
     })
+    #item = ls.item_set.get(id=1)
     # original
     #ls = ToDoList.objects.get(name=name)
-    #item = ls.item_set.get(id=1)
     #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
     #return HttpResponse("<h1>%s</h1> %ls.name")
 
