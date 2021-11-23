aa0533957a93459e424e3a2f540286095e5aa676
ReX342 <occ.rex@gmail.com>
2021-11-23 18:15:03 +0100

    added html to httpresponse argument and copied index def for view 1 def

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(response):
-    return HttpResponse("Tech with Tim")
+    return HttpResponse("<h1>Tech with Tim</h1>")
+#Copy index(response): and rename definition
+def v1(response):
+    return HttpResponse("<h1>View 1</h1>")
