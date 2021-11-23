080642f91327b94c970739c9347f0280b8858d79
ReX342 <occ.rex@gmail.com>
2021-11-23 21:35:14 +0100

    give id 2 another item

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 
 # Create your views here.
-def index(response, name):
+def index(response, id):
     ls = ToDoList.objects.get(name=name)
     item = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" (%ls.name, str(item.text)))
