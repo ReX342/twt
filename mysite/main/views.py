5993bc82741c2721798ed32b94157c141ae75e9a
ReX342 <occ.rex@gmail.com>
2021-11-23 20:05:32 +0100

    nt bug gone somehow

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,5 +4,5 @@ from .models import ToDoList, Item
 
 # Create your views here.
 def index(response, id):
-    ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" %ls.name)
+    #ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" % id)
