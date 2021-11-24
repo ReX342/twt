464bbb9e17ee1149818af259a566e9e87c0e68cd
ReX342 <occ.rex@gmail.com>
2021-11-24 19:04:46 +0100

    make sure we pass the POST data in methodpost

--- mysite/register/views.py
+++ mysite/register/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(response):
     if response.method == "POST":
-        form = UserCreationForm()
+        form = UserCreationForm(response.POST)
         if form.is_valid():
             form.save()
     else:
