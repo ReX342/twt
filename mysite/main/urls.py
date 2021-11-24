791a9592e39a9f8148586d485a6570e501bdbaf6
ReX342 <occ.rex@gmail.com>
2021-11-24 20:24:18 +0100

    broke and finished

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     #path("<str:name>", views.index, name="index"),
     path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
-    path("home", views.home, name="home"),
-    path("create", views.create, name="create"),
+    path("home/", views.home, name="home"),
+    path("create/", views.create, name="create"),
+    path("view/", views.view, name="view"),
 ]
+
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -11,29 +11,31 @@ def index(response, id):
     ls = ToDoList.objects.get(id=id)
     #item = ls.item_set.get(id=1)
     
-    #{"save":["save"], "c1":["clicked"]}
-    if response.method == "POST":
-        print(response.POST)    
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+    
+        #{"save":["save"], "c1":["clicked"]}
+        if response.method == "POST":
+            print(response.POST)    
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()        
+                
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+                
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
-                item.save()        
+                    print("valid")
             
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("valid")
-        
-    return render(response, "main/list.html", {
-        "name":ls.name,
-        "ls": ls
-    })
+        return render(response, "main/list.html", {
+            "ls": ls
+        })
+    return render(response, "main/view.html", {"ls":ls})
     # original
     #ls = ToDoList.objects.get(name=name)
     #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
@@ -51,6 +53,8 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
+
 
         return HttpResponseRedirect("/%i" %t.id)
 
@@ -58,3 +62,6 @@ def create(response):
         form = CreateNewList()
 
     return render(response, "main/create.html", {"form":form})
+
+def view(reponse):
+    return render(response, "main/view.html", {})
