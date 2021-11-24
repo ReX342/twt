24d52f00b8ba96d235d7cabe5d5add5cbccedfb0
ReX342 <occ.rex@gmail.com>
2021-11-24 16:41:14 +0100

    cp from site

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -26,14 +26,15 @@ def home(response):
 def create(response):
     if response.method == "POST":
         form = CreateNewList(response.POST)
-        
+
         if form.is_valid():
             n = form.cleaned_data["name"]
-            t =  ToDoList(name=n)
+            t = ToDoList(name=n)
             t.save()
 
         return HttpResponseRedirect("/%i" %t.id)
 
     else:
         form = CreateNewList()
+
     return render(response, "main/create.html", {"form":form})
